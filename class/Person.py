583775da2dbb370885a573782bfd118a091ff642
EAGLEWANG\lenovo <wz1446228144@mail.ustc.edu.cn>
2023-03-09 22:26:33 +0800

    class

--- class/Person.py
+++ class/Person.py
@@ -1,13 +1,17 @@
 class Person:
+
     bio = "human"
 
-    def __init__(self,name,gender,sex,age,color):
+    def __init__(self,name,gender,sex,age,color,bio):
         self.name = name
         self.gendr = gender
         self.sex = sex
         self.age = age
         self.color = color
+        self.b = bio
     
     def ruSWM(self):
         if self.gendr == 'male' and self.sex == 'male' and self.color=="white":
             print("U r a fucking straight white man")
+        else:
+            print("bro")
